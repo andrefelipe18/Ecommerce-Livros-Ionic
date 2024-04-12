e3bf9e46ed304c91d7bdefe6fbdf1b222287fae9
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-04-12 14:36:28 -0300

    [ADD] Criando Tabs para navegação entre telas

---
+++ components/FooterTabs.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { home, star, card, homeOutline, starOutline, cardOutline} from 'ionicons/icons';
+
+const router = useIonRouter();
+
+const currentRoute = ref(router.route);
+
+const homeIcon = computed(() => {
+    return currentRoute.value === '/' ? homeOutline : home;
+});
+
+const wishListIcon = computed(() => {
+    return currentRoute.value === '/wishlist' ? star : starOutline;
+});
+
+const purchasedIcon = computed(() => {
+    return currentRoute.value === '/purchased' ? card : cardOutline;
+});
+
+const toHome = () => router.push('/');
+const toWishList = () => router.push('/wishlist');
+const toPurchased = () => router.push('/purchased');
+</script>
+<template>
+    <ion-tabs>
+        <ion-router-outlet></ion-router-outlet>
+        <ion-tab-bar slot="bottom">
+            <ion-tab-button tab="home" @click="toHome">
+                <ion-icon :icon="homeIcon" />
+                <ion-label>Home</ion-label>
+            </ion-tab-button>
+
+            <ion-tab-button tab="wishlist" @click="toWishList">
+                <ion-icon :icon="wishListIcon" />
+                <ion-label>Lista de Desejo</ion-label>
+            </ion-tab-button>
+
+            <ion-tab-button tab="purchased" @click="toPurchased">
+                <ion-icon :icon="purchasedIcon" />
+                <ion-label>Comprados</ion-label>
+            </ion-tab-button>
+        </ion-tab-bar>
+    </ion-tabs>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,25 @@
 </script>
 <template>
     <ion-page>
+        <ion-content>
+            <FooterTabs />
+            <ion-header>
+                <ion-toolbar>
+                    <ion-title>Home</ion-title>
+                </ion-toolbar>
+            </ion-header>
 
+            <ion-list>
+                <ion-item>
+                    <ion-label>Item 1</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Item 2</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Item 3</ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-content>
     </ion-page>
 </template>
